d3b448fb536f077c39c6254aabad224209455b0d
Alexander Hansen <19591635+aleha1999@users.noreply.github.com>
2022-04-30 00:54:52 +0200

    Made a custom movement sensor accessory

---
+++ src/customAccessory.ts
@@ -0,0 +1,33 @@
+import { AccessoryConfig, AccessoryPlugin, API, Logging, Service } from 'homebridge';
+
+export class TestAccessory implements AccessoryPlugin {
+
+  log: Logging;
+  config: AccessoryConfig;
+  api: API;
+
+  informationService: Service;
+  movementService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.config = config;
+    this.api = api;
+
+    this.informationService = new this.api.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.api.hap.Characteristic.Manufacturer, 'Custom Manufacturer')
+      .setCharacteristic(this.api.hap.Characteristic.Model, 'Custom Model');
+
+    this.movementService = new this.api.hap.Service.MotionSensor(this.config.name);
+    this.movementService.getCharacteristic(api.hap.Characteristic.MotionDetected)
+      .onGet(this.handleMotionDetected.bind(this));
+  }
+
+  handleMotionDetected() {
+    return 1;
+  }
+
+  getServices(): Service[] {
+    throw new Error('Method not implemented.');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+//import { PLATFORM_NAME } from './settings';
+//import { ExampleHomebridgePlatform } from './platform';
+import { TestAccessory } from './customAccessory';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerAccessory('TestAccessory', TestAccessory);
 };
